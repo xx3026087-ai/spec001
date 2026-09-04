a261a879f26d986a74f6f4de5de6ac191427eb28
xx3026087-ai <xx3026087@gmail.com>
2026-09-04 09:17:24 +0500

    Root build configuration for spec001

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("com.android.library") version "8.1.4" apply false
+    kotlin("android") version "1.9.20" apply false
+    kotlin("jvm") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
